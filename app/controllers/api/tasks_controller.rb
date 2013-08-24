5ae722f1218c6c224bab83c49655676884a8763b
Michal Kwiatkowski <michal@trivas.pl>
2013-08-24 13:35:28 +0200

    Implement update API for tasks.

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -4,12 +4,25 @@ class Api::TasksController < ApplicationController
   end
 
   def create
-    task_list.tasks.create!(params.permit(:description))
+    task_list.tasks.create!(safe_params)
+    render nothing: true
+  end
+
+  def update
+    task.update_attributes(safe_params)
     render nothing: true
   end
 
   private
   def task_list
     TaskList.find_by_id(params[:task_list_id])
+  end
+
+  def task
+    task_list.tasks.find_by_id(params[:id])
+  end
+
+  def safe_params
+    params.permit(:description, :priority, :completed)
   end
 end
--- spec/controllers/api/tasks_controller_spec.rb
+++ spec/controllers/api/tasks_controller_spec.rb
@@ -1,32 +1,30 @@
 require 'spec_helper'
 
 describe Api::TasksController do
-  context "for a new logged-in user" do
-    let(:user) { create(:user) }
+  context "for a logged-in user with two tasks" do
+    let(:task_list) { create(:task_list, :with_tasks) }
+    let(:task1) { task_list.tasks[0] }
+    let(:task2) { task_list.tasks[1] }
+    let(:user) { task_list.owner }
+
     before { sign_in(user) }
 
     describe "#index" do
-      context "for a task list with some tasks" do
-        let(:task_list) { create(:task_list, :with_tasks, owner: user) }
-        let(:task1) { task_list.tasks[0] }
-        let(:task2) { task_list.tasks[1] }
-
-        it "should return json of those tasks" do
-          get :index, task_list_id: task_list.id
-          tasks = JSON.parse(response.body)
-          tasks.should == [
-            {'id' => task1.id, 'description' => task1.description,
-              'priority' => nil, 'due_date' => nil, 'completed' => false},
-            {'id' => task2.id, 'description' => task2.description,
-              'priority' => nil, 'due_date' => nil, 'completed' => false}
-          ]
-        end
+      it "should return json of those tasks" do
+        get :index, task_list_id: task_list.id
+        tasks = JSON.parse(response.body)
+        tasks.should == [
+          {'id' => task1.id, 'description' => task1.description,
+            'priority' => nil, 'due_date' => nil, 'completed' => false},
+          {'id' => task2.id, 'description' => task2.description,
+            'priority' => nil, 'due_date' => nil, 'completed' => false}
+        ]
       end
     end
 
     describe "#create" do
       let(:post_create) do
-        post :create, task_list_id: user.task_list.id, description: "New task"
+        post :create, task_list_id: task_list.id, description: "New task"
       end
 
       it "should add the record to the database" do
@@ -37,6 +35,30 @@ describe Api::TasksController do
 
       it "should return 200 OK" do
         post_create
+        response.should be_success
+      end
+
+      it "should preserve passed parameters" do
+        post_create
+        Task.order(:id).last.description.should == "New task"
+      end
+    end
+
+    describe "#update" do
+      let(:patch_update) do
+        patch :update, task_list_id: task_list, id: task1.id,
+          description: "New description", priority: 1, completed: true
+      end
+
+      it "should update passed parameters of the given task" do
+        patch_update
+        task1.reload.description.should == "New description"
+        task1.priority.should == 1
+        task1.completed.should be_true
+      end
+
+      it "should return 200 OK" do
+        patch_update
         response.should be_success
       end
     end
