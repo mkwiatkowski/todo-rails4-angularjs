29a8be4c3b53a9a66a67e2fee2502de249ce30d9
Michal Kwiatkowski <michal@trivas.pl>
2013-08-24 13:21:30 +0200

    Implement create API for tasks.

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -3,6 +3,11 @@ class Api::TasksController < ApplicationController
     render json: task_list.tasks
   end
 
+  def create
+    task_list.tasks.create!(params.permit(:description))
+    render nothing: true
+  end
+
   private
   def task_list
     TaskList.find_by_id(params[:task_list_id])
--- spec/controllers/api/tasks_controller_spec.rb
+++ spec/controllers/api/tasks_controller_spec.rb
@@ -1,26 +1,43 @@
 require 'spec_helper'
 
 describe Api::TasksController do
-  describe "#index" do
-    context "for user that has a task list with some tasks" do
-      let(:task_list) { create(:task_list, :with_tasks) }
-      let(:task1) { task_list.tasks[0] }
-      let(:task2) { task_list.tasks[1] }
-      let(:user) { task_list.owner }
+  context "for a new logged-in user" do
+    let(:user) { create(:user) }
+    before { sign_in(user) }
 
-      before do
-        sign_in(user)
+    describe "#index" do
+      context "for a task list with some tasks" do
+        let(:task_list) { create(:task_list, :with_tasks, owner: user) }
+        let(:task1) { task_list.tasks[0] }
+        let(:task2) { task_list.tasks[1] }
+
+        it "should return json of those tasks" do
+          get :index, task_list_id: task_list.id
+          tasks = JSON.parse(response.body)
+          tasks.should == [
+            {'id' => task1.id, 'description' => task1.description,
+              'priority' => nil, 'due_date' => nil, 'completed' => false},
+            {'id' => task2.id, 'description' => task2.description,
+              'priority' => nil, 'due_date' => nil, 'completed' => false}
+          ]
+        end
+      end
+    end
+
+    describe "#create" do
+      let(:post_create) do
+        post :create, task_list_id: user.task_list.id, description: "New task"
       end
 
-      it "should return json of those tasks" do
-        get :index, task_list_id: task_list.id
-        tasks = JSON.parse(response.body)
-        tasks.should == [
-          {'id' => task1.id, 'description' => task1.description,
-            'priority' => nil, 'due_date' => nil, 'completed' => false},
-          {'id' => task2.id, 'description' => task2.description,
-            'priority' => nil, 'due_date' => nil, 'completed' => false}
-        ]
+      it "should add the record to the database" do
+        expect {
+          post_create
+        }.to change(Task, :count).by(1)
+      end
+
+      it "should return 200 OK" do
+        post_create
+        response.should be_success
       end
     end
   end
