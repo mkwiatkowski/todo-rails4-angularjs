cf999b39c2e77331b9bff4fe640b28b9653222a5
Gosia Kwidzinska <malgorzata.kwidzinska@gmail.com>
2013-09-06 13:31:29 +0200

    Destroy associated tasks when deleting a list

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -1,6 +1,6 @@
 class TaskList < ActiveRecord::Base
   belongs_to :owner, class_name: User
-  has_many :tasks, -> { order :priority }, foreign_key: :list_id
+  has_many :tasks, -> { order :priority }, foreign_key: :list_id, dependent: :destroy
 
   validates :owner, presence: true
   validates :name, presence: true
